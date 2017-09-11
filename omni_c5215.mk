948748953eca5411bf47ada3224679b3fa86dedb
androidlover5842 <androidlover5842@gmail.com>
2017-09-12 00:22:16 +0530

    rebrand to kyocera

--- omni_c5215.mk
+++ omni_c5215.mk
@@ -5,11 +5,11 @@ PRODUCT_RELEASE_NAME := c5215
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration.
-$(call inherit-product, device/samsung/c5215/c5215.mk)
+$(call inherit-product, device/kyocera/c5215/c5215.mk)
 
 # Device identifier.
 PRODUCT_DEVICE := c5215
 PRODUCT_NAME := omni_c5215
 PRODUCT_BRAND := c5215
 PRODUCT_MODEL := GT-I8552
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_MANUFACTURER := kyocera
